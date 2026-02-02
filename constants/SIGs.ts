32019bf864e3ffefa3c8125b87dc0c1c1187ce36
Aux1r <ThePigZilla@gmail.com>
2026-02-02 15:04:51 +0000

    update evp link

--- constants/SIGs.ts
+++ constants/SIGs.ts
@@ -200,7 +200,7 @@ export const SIGs: SIG[] = [
     },
     backgroundColor: '#3333f5',
     textColor: '#ffffff',
-    websiteURL: 'https://edinburgh-venture-point.webflow.io/',
+    websiteURL: 'https://edinburghventurepoint.com',
     borderColor: '',
     calendarURL:
       '',
